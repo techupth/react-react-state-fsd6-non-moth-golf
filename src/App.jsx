e091503b1bed683d94d559cb0bef72f22a1db4f6
fossi13 <fossi13@users.noreply.github.com>
2024-06-05 12:20:33 +0700

    Fix: add useState and add event Onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
+import { useState } from "react";
+
+
+
 
 function App() {
+  const [text, setText] = useState("Greeting Message")
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{text}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => setText('สวัสดี!')}>สวัสดี!</button>
+        <button onClick={() => setText('Hi!')}>Hi!</button>
+        <button onClick={() => setText('你好!')}>你好!</button>
       </div>
     </div>
   );
